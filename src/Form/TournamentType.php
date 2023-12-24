e35dd84824b60cd07702cf368d6c6bb72e3e60f0
sshmer <sergeyshmer99@gmail.com>
2023-12-24 17:09:15 +0700

    Созданы сущности, миграции, круды

---
+++ src/Form/TournamentType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Tournament;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TournamentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('slug')
+            ->add('matches')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Tournament::class,
+        ]);
+    }
+}
